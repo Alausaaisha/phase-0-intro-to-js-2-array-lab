89cf2b4bee0911b94a2c2cfe22a014256d95e74f
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-15 05:36:03 +0100

    intro to js array lab

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+//const destructivelyAppendCat = cats.push("Ralph")
+//destructivelyAppendCat;
+
+function destructivelyAppendCat(Ralph) {
+  // let cats = ["Milo", "Otis", "Garfield"];
+    Ralph = cats.push("Ralph");
+    return Ralph;
+}
+
+//function Arrayfunctions() {
+   // beforeEach(function () {
+    //  cats.length = 0;
+
+     // cats.push('Milo', 'Otis', 'Garfield');
+    //});
+//}
+
+function destructivelyPrependCat(Bob){
+    Bob = cats.unshift("Bob");
+    return Bob;
+}
+
+function destructivelyRemoveLastCat(removeGarfield){
+    removeGarfield = cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(removeMilo){
+    removeMilo = cats.shift();
+    return cats;
+}
+
+function appendCat(Broom){
+    Broom = [
+        ...cats.slice(0, 3),
+        "Broom"
+    ];
+    return Broom;
+}
+
+function prependCat(Arnold){
+    Arnold = [
+        "Arnold",
+        ...cats.slice(0, 3)
+    ];
+    return Arnold;
+}
+
+function removeLastCat(removeGarfieldInNewarray){
+    removeGarfieldInNewarray = cats.slice(0, -1);
+    return removeGarfieldInNewarray;
+}
+
+function removeFirstCat(removeMiloinNewArray){
+    removeMiloinNewArray = cats.slice(1);
+    return removeMiloinNewArray;
+}
